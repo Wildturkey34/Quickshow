d95157c9e07949bf5937a737b8f24f96b095f8e8
Wildturkey34 <raunakghimire18@gmail.com>
2025-10-15 23:29:49 +0545

    fixed inngest function clerk connection

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -2,12 +2,12 @@ import { Inngest } from "inngest";
 import User from "../models/User.js";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "movie-ticket-app:" });
+export const inngest = new Inngest({ id: "movie-ticket-app" });
 
 // Inngest Function to save user data to a database
 const syncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk" },
-  { event: "clerk/user.created" },
+  { event: "webhook-integration/user.created" },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
@@ -20,20 +20,21 @@ const syncUserCreation = inngest.createFunction(
     await User.create(userData);
   },
 );
-//  Inngest Function to delete user from database
 
+// Inngest Function to delete user from database
 const syncUserDeletion = inngest.createFunction(
   { id: "delete-user-with-clerk" },
-  { event: "clerk/user.deleted" },
+  { event: "webhook-integration/user.deleted" },
   async ({ event }) => {
     const { id } = event.data;
     await User.findByIdAndDelete(id);
   },
 );
 
+// Inngest Function to update user
 const syncUserUpdation = inngest.createFunction(
   { id: "update-user-from-clerk" },
-  { event: "clerk/user.updated" },
+  { event: "webhook-integration/user.updated" },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
